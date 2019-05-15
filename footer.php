ecb631b84306a427883c6a054626979eda25c5a6
Szari <36680669+Szari@users.noreply.github.com>
2019-05-15 22:55:06 +0200

    First page

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+  <div class="footer-copyright text-center py-5 mx-auto">© 2019 Copyright: Kacper Jędrzejewski</div>
+</footer>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include 'header.php'; ?>
+<style>
+<?php include 'main.css'; ?>
+</style>
+<div class = "row">
+  <div class = "col-5 pt-5 pb-5">
+    Między jednym meetingiem, a kolejnym treningiem i motywacyjnym coachingiem
+    jakby mało jest czasu na gotowanie, nieprawda? Nie szkodzi. My mamy czas. I wiedzę.
+    Wiemy, jak policzyć te cholerne kalorie i dopasować dietę, żeby wszystkie Twoje zabiegi,
+    by zyskać życiową formę, nie poszły na marne. My, czyli doświadczeni dietetycy i renomowani kucharze.
+    Wybierz więc swój program i przekonaj się, jak pysznie być fit. Bo, jak twierdzą znawcy:<br>
+    <span class = "font-italic font-weight-bold"><h1>you're never too much fit</h1></span>
+  </div>
+  <div class = "col-7">
+    <img src = "45332.png"  class = "img-responsive pl-5 py-4" width="400">
+  </div>
+</div>
+<div class = "container" id="diety">
+  <div class = "row" id="dietyHeader">
+    <div class = "col-12 text-center display-4"> Nasze diety</div>
+  </div>
+  <div class = "row">
+    <div class = "col-3 text-center">
+      <table class = "table table-hover">
+        <thead>
+          <tr>
+            <th>Nazwa</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Temp</td>
+          </tr>
+          <tr>
+            <td>Temp</td>
+          </tr>
+          <tr>
+            <td>Temp</td>
+          </tr>
+          <tr>
+            <td>Temp</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+<?php include 'footer.php'; ?>
